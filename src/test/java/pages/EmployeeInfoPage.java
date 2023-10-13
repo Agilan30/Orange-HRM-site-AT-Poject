5dda8f84e0546d1af5b960e8f4a425679ce3879f
Agilan T R <74779006+Agilan30@users.noreply.github.com>
2023-10-13 10:42:28 +0530

    Update EmployeeInfoPage.java

--- src/test/java/pages/EmployeeInfoPage.java
+++ src/test/java/pages/EmployeeInfoPage.java
@@ -1,74 +1,31 @@
-package pages;
-import com.github.javafaker.Faker;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import utils.Utils;
+import time
+from faker import Faker
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
-import java.util.List;
+class EmployeeInfoPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.userMenu = driver.find_elements_by_class_name("oxd-main-menu-item")
+        self.btnRadio = driver.find_elements_by_class_name("oxd-radio-input")
+        self.dropdown = driver.find_elements_by_class_name("oxd-select-text-input")
+        self.contactDetails = driver.find_elements_by_class_name("orangehrm-tabs-item")
+        self.txtInput = driver.find_elements_by_class_name("oxd-input")
+        self.dropdownCountry = driver.find_element_by_class_name("oxd-select-text-input")
+        self.Submit = driver.find_elements_by_css_selector("[type=submit]")
 
-public class EmployeeInfoPage {
-    @FindBy(className = "oxd-main-menu-item")
-    public List<WebElement> userMenu;
-    @FindBy(className = "oxd-radio-input")
-    public List<WebElement> btnRadio;
-    @FindBy(className = "oxd-select-text-input")
-    public List<WebElement> dropdown;
-    @FindBy(className = "orangehrm-tabs-item")
-    public List<WebElement> contactDetails;
-    @FindBy(className = "oxd-input")
-    public List<WebElement> txtInput;
-    @FindBy(className = "oxd-select-text-input")
-    public WebElement dropdownCountry;
-    @FindBy(css = "[type=submit]")
-    public List<WebElement> Submit;
-    public EmployeeInfoPage(WebDriver driver) {
+    def selectGender(self):
+        self.btnRadio[1].click()
+        time.sleep(1.5)
+        self.Submit[0].click()
 
-        PageFactory.initElements(driver, this);
-    }
-    public void selectGender() throws InterruptedException {
-
-        btnRadio.get(1).click();
-        Thread.sleep(1500);
-        Submit.get(0).click();
-    }
-    public void selectBloodType() throws InterruptedException {
-        dropdown.get(2).click();
-        dropdown.get(2).sendKeys(Keys.ARROW_DOWN);
-        Thread.sleep(1000);
-        dropdown.get(2).sendKeys(Keys.ARROW_DOWN);
-        Thread.sleep(1000);
-        dropdown.get(2).sendKeys(Keys.ENTER);
-        Thread.sleep(1000);
-        Submit.get(1).click();
-    }
-    public void selectContact() throws InterruptedException {
-        contactDetails.get(1).click();
-        Thread.sleep(1000);
-        Faker faker =new Faker();
-        String streetAddress=faker.address().streetAddress();
-        txtInput.get(1).sendKeys(streetAddress);
-        txtInput.get(2).sendKeys(streetAddress);
-        String city =faker.address().city();
-        txtInput.get(3).sendKeys(city);
-        String state=faker.address().state();
-        txtInput.get(4).sendKeys(state);
-        String postalCode=faker.address().zipCode();
-        txtInput.get(5).sendKeys(postalCode);
-        Thread.sleep(1000);
-        dropdownCountry.click();
-        dropdownCountry.sendKeys(Keys.ARROW_DOWN);
-        Thread.sleep(1000);
-        dropdownCountry.sendKeys(Keys.ARROW_DOWN);
-        Thread.sleep(1000);
-        dropdownCountry.sendKeys(Keys.ENTER);
-        Thread.sleep(1000);
-        txtInput.get(7).sendKeys("+18143008329");
-        String email = "user"+Utils.generateRandomNumber(100, 999)+"@gmail.com";
-        txtInput.get(9).sendKeys(email);
-        Thread.sleep(1000);
-        Submit.get(0).click();
-    }
-}
+    def selectBloodType(self):
+        dropdown = Select(self.dropdown[2])
+        dropdown.select_by_index(1)
+        time.sleep(1)
+        dropdown.select_by_index(2)
+        time.sleep(1)
+        dropdown.select_by_index(3)
